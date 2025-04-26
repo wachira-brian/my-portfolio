c2b76c0a7c90bf5e4f84e570c2430b894c0126b4
Brian Kariuki Wachira <73661988+wachira-brian@users.noreply.github.com>
2025-04-26 08:20:29 +0300

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -113,7 +113,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://github.com/wachira-brian/ecommerce-app"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
